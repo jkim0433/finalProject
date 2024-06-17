faeabcd4a27e7b5f781173a9063d2e29896b383b
DinDjarin247 <space1429@gmail.com>
2024-06-17 15:40:35 +0900

    24.06.17

---
+++ rosario/src/main/java/com/example/rosario/RosarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.rosario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosarioApplication.class, args);
+	}
+
+}
---
+++ rosario/src/test/java/com/example/rosario/RosarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rosario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
