01bcf1919f80b108b4e37d13a122370e23a8667b
kwakdh <kwakdh0106@naver.com>
2024-06-18 09:37:56 +0900

    @NotNull, Min 사용 가능

--- rosario/src/main/java/com/example/rosario/entity/Product.java
+++ rosario/src/main/java/com/example/rosario/entity/Product.java
@@ -1,6 +1,9 @@
 package com.example.rosario.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,21 +20,21 @@ public class Product {
     private Long productId;         // 상품ID
 
 
-    //@NotBlank(message = "상품명은 필수 입력 항목입니다.")
+    @NotBlank(message = "상품명은 필수 입력 항목입니다.")
     @Column(nullable = false)
     private String productNm;       // 상품명
 
-    //@NotNull(message = "상품가격은 필수 입력 항목입니다.")
-    //@Min(value = 0, message = "상품가격은 0 이상이어야 합니다.")
+    @NotNull(message = "상품가격은 필수 입력 항목입니다.")
+    @Min(value = 0, message = "상품가격은 0 이상이어야 합니다.")
     @Column(nullable = false)
     private Long productPrice;      // 상품가격
 
-    //@NotNull(message = "상품재고수량은 필수 입력 항목입니다.")
-    //@Min(value = 0, message = "상품재고수량은 0 이상이어야 합니다.")
+    @NotNull(message = "상품재고수량은 필수 입력 항목입니다.")
+    @Min(value = 0, message = "상품재고수량은 0 이상이어야 합니다.")
     @Column(nullable = false)
     private Long productStock;      // 상품재고수량
 
-    //@NotBlank(message = "상품사이즈는 필수 입력 항목입니다.")
+    @NotBlank(message = "상품사이즈는 필수 입력 항목입니다.")
     @Column(nullable = false)
     private String productSize;     // 상품사이즈 (M, L, XL)
 }
