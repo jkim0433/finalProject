0c4ffc6ee4d17fb43253d16a51b5b1590e4dfe57
kimbibim <pumpkin920902@gmail.com>
2024-06-20 09:57:31 +0900

    0620 언니꺼 merge중

--- rosario_front/src/App.jsx
+++ rosario_front/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./log-in/LoginPage";
 import RegisterPage from "./log-in/RegisterPage";
+// import Dashboard from "./sellernav/Dashboard";
+import Sellernav from "./sellernav/Sellernav";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/loginpage" element={<LoginPage />} />
         <Route path="/api/customers/register" element={<RegisterPage />} />
+        <Route path="/admin/sellernav" element={<Sellernav />} />
       </Routes>
     </Router>
   );
