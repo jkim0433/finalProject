ca16b3048d8dc23970e46ed9b498fb6a65f1e92b
kwakdh <kwakdh0106@naver.com>
2024-06-27 10:32:19 +0900

    수정사항포함

--- rosario_front/src/sellerorders/SellerCalendar.jsx
+++ rosario_front/src/sellerorders/SellerCalendar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import "./SellerCalendar.module.css";
+// import "./SellerCalendar.module.css";
 
 const localizer = momentLocalizer(moment);
 
