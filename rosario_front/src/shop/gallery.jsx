3360b3fbc5fb0cc460e0f167e0c6ad6bad2eeee7
kwakdh <kwakdh0106@naver.com>
2024-06-27 10:33:14 +0900

    Gallery확인

--- rosario_front/src/shop/gallery.jsx
+++ rosario_front/src/shop/gallery.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import GallerySecond from '../selleredit/GellarySecond';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import GallerySecond from "../selleredit/GellarySecond";
 
 const Gallery = ({ productId }) => {
   const [photos, setPhotos] = useState([]);
@@ -8,14 +8,16 @@ const Gallery = ({ productId }) => {
   useEffect(() => {
     const fetchProductImages = async () => {
       try {
-        const response = await axios.get(`http://localhost:8081/api/uploadProdImg/${productId}`);
+        const response = await axios.get(
+          `http://localhost:8081/api/uploadProdImg/${productId}`
+        );
         if (Array.isArray(response.data)) {
           setPhotos(response.data);
         } else {
-          console.error('Invalid data format from API');
+          console.error("Invalid data format from API");
         }
       } catch (error) {
-        console.error('Error fetching product images:', error);
+        console.error("Error fetching product images:", error);
       }
     };
 
@@ -25,9 +27,11 @@ const Gallery = ({ productId }) => {
   return (
     <div>
       <h2>Product Gallery</h2>
-      <GallerySecond photos={photos.map(img => img.prodFilePath)} />
+      <GallerySecond photos={photos.map((img) => img.prodFilePath)} />
     </div>
   );
 };
 
 export default Gallery;
+
+// 수정확인
