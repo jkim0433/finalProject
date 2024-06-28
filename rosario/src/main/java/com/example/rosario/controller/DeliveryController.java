c9ec132c6712fc2fe1cfc2fbf50bbc97da4d614b
kimbibim <pumpkin920902@gmail.com>
2024-06-28 10:59:27 +0900

    도경(주문 백엔드 작성, 사용안한파일 삭제)

---
+++ rosario/src/main/java/com/example/rosario/controller/DeliveryController.java
@@ -0,0 +1,31 @@
+package com.example.rosario.controller;
+
+import com.example.rosario.entity.Delivery;
+import com.example.rosario.service.DeliveryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/deliveries")
+public class DeliveryController {
+
+    @Autowired
+    private DeliveryService deliveryService;
+
+    @GetMapping
+    public List<Delivery> getAllDeliveries(){
+        return deliveryService.getAllDeliveries();
+    }
+
+    @GetMapping("/{deliveryId}")
+    public Delivery getDeliveryById(@PathVariable Long deliveryId){
+        return deliveryService.getDeliveryById(deliveryId);
+    }
+
+    //Other endpoints for CRUD operations
+}
---
+++ rosario/src/main/java/com/example/rosario/repository/DeliveryRepository.java
@@ -0,0 +1,8 @@
+package com.example.rosario.repository;
+
+import com.example.rosario.entity.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+
+}
